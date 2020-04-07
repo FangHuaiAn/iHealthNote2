f389de5757a569677a33ff002a72f150cc3745a4
Fang Huai An <liddle.fang@gmail.com>
2020-04-07 15:48:16 +0800

    Initial Commit

---
+++ iHealthNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iHealthNote
+//
+//  Created by 房懷安 on 2020/4/7.
+//  Copyright © 2020 房懷安. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
